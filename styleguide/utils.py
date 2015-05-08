cd181ee28f423e5b06f0025d16ef60aa940150c3
Colin Copeland <copelco@caktusgroup.com>
2015-05-08 11:42:10 -0400

    utils to help find styleguide templates

---
+++ styleguide/utils.py
@@ -0,0 +1,24 @@
+import os
+from django import template
+
+
+def get_styleguide_templates():
+    template_paths = []
+    for template_dir in get_styleguide_dirs():
+        for (dirpath, dirnames, filenames) in os.walk(template_dir):
+            template_paths.extend(filenames)
+    return template_paths
+
+
+def get_styleguide_dirs():
+    """Find all styleguide/ dirs within all Django template dirs"""
+    template_dirs = []
+    engines = template.engines.all()
+    for engine in engines:
+        # Reverse template_loaders to search apps first, followed by project
+        # templates, so the built-in styleguide templates are list first.
+        for loader in reversed(engine.engine.template_loaders):
+            for template_dir in loader.get_template_sources('styleguide'):
+                if os.path.isdir(template_dir):
+                    template_dirs.append(template_dir)
+    return template_dirs
