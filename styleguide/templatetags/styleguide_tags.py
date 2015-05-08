7f7dff6992a061af468c540a0888a2d3fd6acb96
Calvin Spealman <calvin@caktusgroup.com>
2015-05-08 15:01:27 -0400

    Form styleguide patterns and more example helpers to track unstyled pieces

--- styleguide/templatetags/styleguide_tags.py
+++ styleguide/templatetags/styleguide_tags.py
@@ -9,31 +9,53 @@ register = template.Library()
 
 @register.tag(name="example")
 def do_example(parser, token):
-    tag_name, lang = token.contents.split(None, 1)
+    tag_name, *args = token.split_contents()
+    args = list(args)
+    kwargs = {}
+    def decode(value):
+        if isinstance(value, str):
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
+        return value
+    while args and '=' in args[-1]:
+        key, value = args.pop(-1).split('=', 1)
+        kwargs[key] = decode(value)
+    args = [decode(arg) for arg in args]
     nodelist = parser.parse(('endexample',))
     parser.delete_first_token()
-    return ExampleNode(lang, nodelist)
+    return ExampleNode(args, kwargs, nodelist)
 
 class ExampleNode(template.Node):
-    def __init__(self, lang, nodelist):
-        self.lang = lang
+    def __init__(self, args, kwargs, nodelist):
+        self.args = args
+        self.kwargs = kwargs
         self.nodelist = nodelist
         super(ExampleNode, self).__init__()
 
     def render(self, context):
+        return self.do_render(*self.args, **self.kwargs)
+
+    def do_render(self, header="", lang='html', status=None):
         output = []
 
         code = self.nodelist.render({})
         code = dedent(code).strip()
 
+
+        if header or status:
+            output.append("<h4 class='%s'>%s</h4>" % (
+                (" styleguide-status-"+status if status else ""),
+                header,
+            ))
         output.append("<div class=styleguide-example>")
         output.append("<div class=styleguide-code>")
 
-        output.append("<pre><code class=%s>" % self.lang)
+        output.append("<pre><code class=%s>" %  lang)
         output.append(defaultfilters.force_escape(code))
         output.append("</code></pre>")
 
         output.append("</div>")
+        output.append("<div class=styleguide-sep><span>➵</span></div>")
         output.append("<div class=styleguide-demo>")
 
         output.append(code)
