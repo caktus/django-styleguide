73ebc99fa0dee3098cb0fa9a65ebb69e62bb06ea
Calvin Spealman <calvin@caktusgroup.com>
2016-02-22 13:03:41 -0500

    Fixes to #9 #10 and small style fixes

--- styleguide/urls.py
+++ styleguide/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url, patterns
 from . import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$',
         views.styleguide,
         name='styleguide'),
     url(r'^(?P<name>[0-9A-Za-z_\-]+)/$', views.styleguide_page, name="styleguide_page"),
-)
+]
