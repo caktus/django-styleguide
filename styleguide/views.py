2e11391291b0cb3a4d1e7150d7f6d8b58a91d4d0
Calvin Spealman <calvin@caktusgroup.com>
2017-12-12 13:00:35 -0500

    Small updates fixing stuff after merge

--- styleguide/views.py
+++ styleguide/views.py
@@ -5,12 +5,16 @@ from django import forms
 class ExampleForm(forms.Form):
     text = forms.CharField()
     disabled_text = forms.CharField(disabled=True)
+    readonly_text = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly':'readonly'})
+    )
     checkbox1 = forms.BooleanField()
     checkbox2 = forms.BooleanField()
     select = forms.ChoiceField(choices=[('',  "Select an Option"), (1, 'one'), (2, 'two'), (3, 'three')])
     radio = forms.ChoiceField(choices=[(1, 'one'), (2, 'two'), (3, 'three')], widget=forms.RadioSelect())
 
 form_initial = {
+    "text": "",
     "disabled_text": "This field can't be changed",
 }
 
