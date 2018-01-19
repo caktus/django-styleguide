eddd79d9265cb48e87c45075aad51701afa04b13
philtuskha <phil.tuskha@gmail.com>
2018-01-19 12:33:51 -0500

    minimal form functionaity

--- styleguide/views.py
+++ styleguide/views.py
@@ -7,12 +7,21 @@ def styleguide(request):
     })
 
 def styleguide_page(request, name):
+    if request.method == 'POST':
+        create_user_form = CreateUserForm(request.POST, initial=create_user_form_initial)
+        if create_user_form.is_valid():
+            pass
+    else:
+        create_user_form = CreateUserForm(initial=create_user_form_initial)
+
     return render(request, "styleguide/styleguide-%s.html" % name, {
         "everything_bagel_form": EverythingBagelForm(initial=everything_bagel_form_initial),
-        "create_user_form": CreateUserForm(initial=create_user_form_initial),
+        "create_user_form": create_user_form,
     })
+
 def styleguide_sub_page(request, name, sub_page):
     return render(request, "styleguide/styleguide-%s-%s.html" % (name, sub_page), {
         "everything_bagel_form": EverythingBagelForm(initial=everything_bagel_form_initial),
         "create_user_form": CreateUserForm(initial=create_user_form_initial),
     })
+
